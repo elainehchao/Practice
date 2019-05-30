aa1ec0e6ee24eda57e95fbe57d4035bd05445137
elainehchao <elainehchao@gmail.com>
2019-05-29 22:03:31 -0700

    Adding removeValue method and tests

--- com/prep/Trees/BinarySearchTree.java
+++ com/prep/Trees/BinarySearchTree.java
@@ -22,6 +22,7 @@ public class BinarySearchTree extends BinaryTree{
 		return containsValueHelper(value, root);
 	}
 	
+	// O(n) because unbalanced
 	public boolean containsValueHelper(Integer value, BinaryTreeNode current) {
 		if (current == null) {
 			return false;
@@ -41,6 +42,7 @@ public class BinarySearchTree extends BinaryTree{
 		return false;
 	}
 
+	// O(n) because unbalanced
 	@Override
 	public void insertValue(Integer element) {
 		if (root == null) {
@@ -70,8 +72,68 @@ public class BinarySearchTree extends BinaryTree{
 	
 	@Override
 	public void removeValue(Integer element) {
-		// TODO Auto-generated method stub
+		removeValueHelper(element, null, root);
+	}
+	
+	private void removeValueHelper(Integer element, BinaryTreeNode parent, BinaryTreeNode current) {
+		if (current == null) {
+			return;
+		}
 		
+		if (current.getValue() == element) {
+			if (current.getRight() != null && current.getLeft() != null) {
+				// find smallest element in right subtree
+				BinaryTreeNode smallest = findAndRemoveSmallest(current, current.getRight());
+				current.setValue(smallest.getValue());
+			} else if (current.getRight() == null){
+				if (parent != null) {
+					if (parent.getLeft() == current) {
+						parent.setLeft(current.getLeft());
+					} else if (parent.getRight() == current) {
+						parent.setRight(current.getLeft());
+					}
+				} else {
+					// root to be deleted
+					root = current.getLeft();
+				}
+			} else if (current.getLeft() == null) {
+				if (parent != null) {
+					if (parent.getLeft() == current) {
+						parent.setLeft(current.getRight());
+					} else if (parent.getRight() == current) {
+						parent.setRight(current.getRight());
+					}
+				} else {
+					// root to be deleted
+					root = current.getRight();
+				}
+			}
+		}
+		
+		if (element < current.getValue()) {
+			removeValueHelper(element, current, current.getLeft());
+		}
+		
+		if (element > current.getValue()) {
+			removeValueHelper(element, current, current.getRight());
+		}
+	}
+	
+	private BinaryTreeNode findAndRemoveSmallest(BinaryTreeNode originalParent, BinaryTreeNode current) {
+		if (current.getLeft() == null) {
+			BinaryTreeNode smallest = current;
+			originalParent.setRight(current.getRight() == null ? null : current.getRight());
+			return smallest;
+		}
+		
+		
+		if (current.getLeft() != null) {
+			BinaryTreeNode smallest = current.getLeft();
+			current.setLeft(current.getLeft().getRight() == null ? null : current.getLeft().getRight());
+			return smallest;
+		}
+		
+		return findAndRemoveSmallest(originalParent, current.getLeft());
 	}
 
 }
--- com/prep/Trees/BinarySearchTreeTest.java
+++ com/prep/Trees/BinarySearchTreeTest.java
@@ -50,5 +50,127 @@ public class BinarySearchTreeTest {
 		assertTrue(tree.containsValue(2));
 		assertFalse(tree.containsValue(100));
 	}
+	
+	@Test
+	public void testRemoveValueRightChildNull() {
+		/*
+		 *          10
+		 *        4    20
+		 *       2       65 
+		 *        3
+		 */
+		
+		BinarySearchTree tree = new BinarySearchTree();
+		tree.insertValue(10);
+		tree.insertValue(4);
+		tree.insertValue(20);
+		tree.insertValue(65);
+		tree.insertValue(2);
+		tree.insertValue(3);
+		tree.removeValue(4);
+		
+		/*
+		 *          10
+		 *        2    20
+		 *         3     65 
+		 *        
+		 */
+		System.out.println("PREORDER");
+		tree.preOrderTraversal();
+		System.out.println("INORDER");
+		tree.inOrderTraversal();
+	}
+	
+	@Test
+	public void testRemoveValueLeftChildNull() {
+		/*
+		 *          10
+		 *        4    20
+		 *       2       65 
+		 *        3
+		 */
+		
+		BinarySearchTree tree = new BinarySearchTree();
+		tree.insertValue(10);
+		tree.insertValue(4);
+		tree.insertValue(20);
+		tree.insertValue(65);
+		tree.insertValue(2);
+		tree.insertValue(3);
+		tree.removeValue(20);
+		
+		/*
+		 *          10
+		 *        4    65
+		 *       2        
+		 *        3
+		 */
+		System.out.println("PREORDER");
+		tree.preOrderTraversal();
+		System.out.println("INORDER");
+		tree.inOrderTraversal();
+	}
+	
+	@Test
+	public void testRemoveValueBothChildrenNotNull() {
+		/*
+		 *          10
+		 *       4     20
+		 *      2    15   65 
+		 *       3
+		 */
+		
+		BinarySearchTree tree = new BinarySearchTree();
+		tree.insertValue(10);
+		tree.insertValue(4);
+		tree.insertValue(20);
+		tree.insertValue(15);
+		tree.insertValue(65);
+		tree.insertValue(2);
+		tree.insertValue(3);
+		tree.removeValue(20);
+		
+		/*
+		 *          10
+		 *        4    65
+		 *       2    15    
+		 *        3
+		 */
+		System.out.println("PREORDER");
+		tree.preOrderTraversal();
+		System.out.println("INORDER");
+		tree.inOrderTraversal();
+	}
+	
+	@Test
+	public void testRemoveRoot() {
+		/*
+		 *          10
+		 *       4     20
+		 *      2    15   65 
+		 *       3
+		 */
+		
+		BinarySearchTree tree = new BinarySearchTree();
+		tree.insertValue(10);
+		tree.insertValue(4);
+		tree.insertValue(20);
+		tree.insertValue(15);
+		tree.insertValue(65);
+		tree.insertValue(2);
+		tree.insertValue(3);
+		tree.removeValue(10);
+		
+		/*
+		 *          15
+		 *        4    20
+		 *       2       65 
+		 *        3
+		 */
+		System.out.println("PREORDER");
+		tree.preOrderTraversal();
+		System.out.println("INORDER");
+		tree.inOrderTraversal();
+	}
 
 }
